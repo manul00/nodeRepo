cd32c8c555e768c95f0516ddc82605752b60050d
manul00 <nulorincon@gmail.com>
2019-10-25 06:07:17 -0300

    agregada ruta de logeo

--- index.js
+++ index.js
@@ -110,6 +110,17 @@ app.delete('//delete/:fecha',(req,res)=>{
 			})
 		});
 });
+
+app.get('/',(req,res)=>{
+
+		adminSchema.find({admin:'admin'}).exec((e,resul)=>{
+			if(e) return res.status(500).send({mesage: 'error al retornar los datos'});
+			if(!resul) return res.status(404).send({mesage: 'no se encontro el usuario'});
+
+			return res.status(200).send({resul})
+		});
+
+});
 //fin de rutas **********************************
 
 
